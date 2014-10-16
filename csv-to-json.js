6de74dedd7396573456287c1d6dd624edb62615c
petercode1 <pr.gail@comcast.net>
2014-10-16 11:24:31 -0600

    Completed bonus assignment

--- csv-to-json.js
+++ csv-to-json.js
@@ -1,30 +1,38 @@
 var fs = require('fs');
 
+/**
+ * Convert CSV to JSON string
+ * @return {JSON string} Array of stringified objects
+ */
 var conversion = function () {
-
 
 	var fileString = process.argv[2];
 
-console.log(fileString);
+// console.log(fileString);
 
 	fs.readFile(fileString, "utf-8", function(err, data) {
-
-console.log(data);
-
+// console.log(data);
 		var csvLineArray = data.split('\n');
-
 		var keysArray = csvLineArray.shift().split(',');
+// console.log("Output")
+// console.log(keysArray);
+		var output = csvLineArray.map(function(person) {
+			var valuesArray = person.split(',');
+			var tempObject = {};
+			for (var i = 0; i < keysArray.length; i++) {
+				tempObject[keysArray[i]] = valuesArray[i];
+			}
+			return tempObject;
+		});
 
-		var valuesArray = csvLineArray.split('');
 
-console.log(csvLineArray);
-
-		var name = keysArray[0];
-		var age = keysArray[1];
-		var city = keysArray[2];
-
-console.log(keysArray);
-
+		fs.writeFile(process.argv[3], JSON.stringify(output), "utf-8", function() {			
+			console.log(JSON.stringify(output));
+		});
 	});
 };
+
+// Execute the function
 conversion();
+
+
--- output.js
+++
@@ -1,5 +0,0 @@
-var csvToJSON = require('./csv-to-json.js');
-
-var conversion = csvToJSON.conversion;
-
-console.log(conversion());
