4098221a3bb9a521a07d0b24e1aab003c79a8ae9
petercode1 <pr.gail@comcast.net>
2014-10-17 17:54:35 -0600

    Experimented with slide functionality for small menus

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).on('ready', function() {
+
+	$('.pop-up').toggle();
+
+	$('.pop-up-toggle').on('click', function() {
+		$('.pop-up').slideToggle(900);
+	});
+
+
+	// LOOK UP JQUERY UI FOR OTHER SLIDE FEATURES
+
+});
