e2d9a2a0edfe546af137d01999eb23a35b442150
petercode1 <pr.gail@comcast.net>
2014-10-31 10:00:45 -0600

    Added 3d-animations

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 $(document).on('ready', function() {
 
+	var reset = function () {
+		TweenMax.set($('#box'), {clearProps: 'all'});
+	};
+
 	CSSPlugin.defaultTransformPerspective = 500;
 
 	$('#card-flip').on('click', function(){
-		// TweenMax.to(box, 9, {rotationY: 360, transformOrigin: ""})
+		TweenMax.to(box, 6, {rotationY: 360, transformOrigin: "", onComplete: reset});
 	});
 
 
 	$('#axis-card-flip').on('click', function(){
-		// TweenMax.to(box, 3, {rotationY: 360, transformOrigin: "left top"})
+		TweenMax.to(box, 3, {rotationY: 360, transformOrigin: "left top", onComplete: reset});
 	});
 
 
-	$('#axis-card-flip').on('click', function(){
-		TweenMax.to(box, 3, {rotation: "-170_short", rotationY: "-=360_cw", rotationX: "1.5rad_cw", onComplete: myFunction});
-		function myFunction () {
-			TweenMax.killAll();
-		
+	$('#orbiting').on('click', function(){
+		TweenMax.to(box, 4, {rotationY: 360, transformOrigin: "left 50% -200", onComplete: reset});
+	});
+
+
+	$('#crazy-motion').on('click', function(){
+		TweenMax.to(box, 3, {rotation: "-170_short", rotationY: "-=360_cw", rotationX: "1.5rad_cw", onComplete: reset});
 	});
 
 	$('#').on('click', function(){});
+
+
   
 
 }); // jQuery
