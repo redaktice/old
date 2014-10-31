c9b210a7b50850bed40ff800294f404db0ef2648
petercode1 <pr.gail@comcast.net>
2014-10-30 23:18:18 -0600

    Set-up 3d animations outline

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 $(document).on('ready', function() {
+
+	CSSPlugin.defaultTransformPerspective = 500;
+
+	$('#card-flip').on('click', function(){
+		// TweenMax.to(box, 9, {rotationY: 360, transformOrigin: ""})
+	});
+
+
+	$('#axis-card-flip').on('click', function(){
+		// TweenMax.to(box, 3, {rotationY: 360, transformOrigin: "left top"})
+	});
+
+
+	$('#axis-card-flip').on('click', function(){
+		TweenMax.to(box, 3, {rotation: "-170_short", rotationY: "-=360_cw", rotationX: "1.5rad_cw", onComplete: myFunction});
+		function myFunction () {
+			TweenMax.killAll();
+		
+	});
+
+	$('#').on('click', function(){});
   
-});
+
+}); // jQuery
